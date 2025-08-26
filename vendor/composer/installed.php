3a3a961b0d9861d154dc89cb0c5253cdc8dd895b
Troy Chaplin <troy.chaplin@gmail.com>
2025-08-25 22:32:25 -0400

    part four: implement namespacing and autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-dev-blog/refactor-multi-block-plugin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e4d149b111262aa292edcd22d75a3a4c1c568bba',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wp-dev-blog/refactor-multi-block-plugin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e4d149b111262aa292edcd22d75a3a4c1c568bba',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
